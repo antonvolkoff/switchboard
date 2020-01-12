2b747cc0d160313750fc5e0b6191f9402d808a82
Anton <choixer@gmail.com>
2020-01-12 17:52:34 +0100

    Setup project for Heroku deployment

--- packages/switchboard/index.ts
+++ packages/switchboard/index.ts
@@ -1,4 +1,4 @@
-import { GraphQLServer } from 'graphql-yoga';
+import { GraphQLServer, Options } from 'graphql-yoga';
 import * as path from 'path';
 
 import Query from './resolvers/query';
@@ -8,6 +8,8 @@ const typeDefs = path.join(__dirname, 'schema.graphql');
 const resolvers = { Query, Mailbox };
 
 const server = new GraphQLServer({ typeDefs, resolvers });
-server.start(function() {
-  console.log('Server running on localhost:4000');
-});
+const webServerOptions: Options = {
+  port: (process.env.PORT || 4000),
+};
+
+server.start(webServerOptions);
