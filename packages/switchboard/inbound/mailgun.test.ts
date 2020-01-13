fb6235075c0838a2a1f0b345c0683ab9d02da0d5
Anton <choixer@gmail.com>
2020-01-13 22:49:23 +0100

    Send request to Outpostr with email content

--- packages/switchboard/inbound/mailgun.test.ts
+++ packages/switchboard/inbound/mailgun.test.ts
@@ -1,19 +1,47 @@
 import * as express from "express";
 import * as supertest from "supertest";
+import { createTransport } from "nodemailer";
+import { Options as MailOptions } from "nodemailer/lib/mailer";
 import { mailgunHandler } from "./mailgun";
 
-test("should register an endpoint", async (done) => {
+async function generateTestEmail(mailOptions: MailOptions): Promise<string> {
+  const transporter = createTransport({
+    streamTransport: true,
+    newline: "unix",
+    buffer: true,
+  });
+
+  const mail = await transporter.sendMail(mailOptions);
+  return mail.message.toString();
+}
+
+test("should handle RFC2822 message", async (done) => {
   const app = express();
-  app.post(
-    "/inbound/mailgun",
-    express.urlencoded({ extended: true }),
-    mailgunHandler()
-  );
+  const middleware = express.urlencoded({ extended: true });
+  app.post("/inbound/mailgun.mime", middleware, mailgunHandler());
+
+  const webhookMeta = {
+    timestamp: "1578936122",
+    token: "438442021526ddf1ca178addfb4e35ea9178d511ae9192c1ad",
+    signature: "87390062f5c9601e47ab15e4866626fa6b7d0e96aa0c3ee810aed32fc8c5f55d"
+  };
+  const envelope = {
+    recipient: "test@mail.outpostr.com",
+    sender: "choixer@gmail.com",
+  }
+  const emailContent = await generateTestEmail({
+    from: "Anton Volkov <choixer@gmail.com>",
+    to: "test@mail.outpostr.com",
+    subject: "Test Email",
+    text: "Hello, World!",
+  });
 
   const response = await supertest(app)
-    .post("/inbound/mailgun")
-    .send("name=anton&surname=volkov")
-    .set("Content-Type", "application/x-www-form-urlencoded");
+    .post("/inbound/mailgun.mime")
+    .type("form")
+    .send(webhookMeta)
+    .send(envelope)
+    .send({ "body-mime": emailContent });
 
   expect(response.status).toEqual(200);
   expect(response.body).toEqual({ok: true});
--- packages/switchboard/inbound/mailgun.ts
+++ packages/switchboard/inbound/mailgun.ts
@@ -1,9 +1,50 @@
 import * as express from "express";
+import axios from "axios";
+import { resolve } from "dns";
+
+interface InputParams {
+  timestamp: number;
+  token: string;
+  signature: string;
+
+  recipient: string;
+  sender: string;
+
+  "body-mime": string;
+}
+
+interface OutpostParams {
+  envelope: { from: string, to: string[] };
+  message: { data: string };
+}
+
+function toOutpostParams(input: InputParams): OutpostParams {
+  return {
+    envelope: {
+      from: input.sender,
+      to: [input.recipient],
+    },
+    message: {
+      data: input["body-mime"],
+    },
+  };
+}
 
 export function mailgunHandler() {
-  return function(req: express.Request, res: express.Response) {
-    console.log(req.headers);
-    console.log(req.body);
-    res.send({ok: true});
+  const httpClient = axios.create({
+    baseURL: "https://www.outpostr.com",
+    timeout: 1000,
+  });
+
+  return async function(req: express.Request, res: express.Response) {
+    const params = <InputParams>req.body;
+    const outpostParams = toOutpostParams(params);
+
+    const response = await httpClient.post('/api/mails', outpostParams);
+    if (response.status == 200) {
+      res.status(200).send({ ok: true });
+    } else {
+      res.status(500).send({ ok: false });
+    }
   };
-};
+}
